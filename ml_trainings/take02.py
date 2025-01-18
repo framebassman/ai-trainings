237c1c1c62a84d30a9257f8073c04d9587119711
Dmitry Romashov <framebassman@gmail.com>
2025-01-18 15:35:17 +0100

    Add takes

---
+++ ml_trainings/take02.py
@@ -0,0 +1,10 @@
+from ml_trainings import get_completion
+
+prompt = f"""
+Generate a list of three made-up book titles along \ 
+with their authors and genres. 
+Provide them in JSON format with the following keys: 
+book_id, title, author, genre.
+"""
+response = get_completion(prompt)
+print(response)
