120abf2299da966488595292d23bc2f54c3c7172
Dmitry Romashov <framebassman@gmail.com>
2025-01-18 15:54:41 +0100

    Fix

--- ml_trainings/guidelines/take04.py
+++ ml_trainings/guidelines/take04.py
@@ -29,3 +29,25 @@ Text:
 response = get_completion(prompt_1)
 print("Completion for prompt 1:")
 print(response)
+
+prompt_2 = f"""
+Your task is to perform the following actions: 
+1 - Summarize the following text delimited by 
+  <> with 1 sentence.
+2 - Translate the summary into French.
+3 - List each name in the French summary.
+4 - Output a json object that contains the 
+  following keys: french_summary, num_names.
+
+Use the following format:
+Text: <text to summarize>
+Summary: <summary>
+Translation: <summary translation>
+Names: <list of names in summary>
+Output JSON: <json with summary and num_names>
+
+Text: <{text}>
+"""
+response = get_completion(prompt_2)
+print("\nCompletion for prompt 2:")
+print(response)
