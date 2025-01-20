a91a6a9798b06a6ae687802d4ff0dc83ced71768
Dmitry Romashov <framebassman@gmail.com>
2025-01-20 11:58:21 +0100

    Add some stuff

---
+++ ml_trainings/chatgpt_prompt_eng/e_translations/take01.py
@@ -0,0 +1,51 @@
+from ml_trainings.chatgpt_prompt_eng import get_completion
+
+prompt = f"""
+Translate the following English text to Spanish: \ 
+```Hi, I would like to order a blender```
+"""
+response = get_completion(prompt)
+print(response)
+
+prompt = f"""
+Tell me which language this is: 
+```Combien coûte le lampadaire?```
+"""
+response = get_completion(prompt)
+print(response)
+
+prompt = f"""
+Translate the following  text to French and Spanish
+and English pirate: \
+```I want to order a basketball```
+"""
+response = get_completion(prompt)
+print(response)
+
+prompt = f"""
+Translate the following text to Spanish in both the \
+formal and informal forms: 
+'Would you like to order a pillow?'
+"""
+response = get_completion(prompt)
+print(response)
+
+user_messages = [
+  "La performance du système est plus lente que d'habitude.",  # System performance is slower than normal
+  "Mi monitor tiene píxeles que no se iluminan.",              # My monitor has pixels that are not lighting
+  "Il mio mouse non funziona",                                 # My mouse is not working
+  "Mój klawisz Ctrl jest zepsuty",                             # My keyboard has a broken control key
+  "我的屏幕在闪烁"                                               # My screen is flashing
+]
+
+for issue in user_messages:
+    prompt = f"Tell me what language this is: ```{issue}```"
+    lang = get_completion(prompt)
+    print(f"Original message ({lang}): {issue}")
+
+    prompt = f"""
+    Translate the following  text to English \
+    and Korean: ```{issue}```
+    """
+    response = get_completion(prompt)
+    print(response, "\n")
